b06cdf90940f46c0bbe8ad977a39c6da630b9baf
jesterhazy <jonathan.esterhazy@gmail.com>
2019-05-07 13:40:01 -0700

    add build-all.sh, publish-all.sh scripts (#35)

---
+++ scripts/build-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Build all the docker images.
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+${DIR}/build.sh --version 1.11.1 --arch cpu
+${DIR}/build.sh --version 1.11.1 --arch gpu
+${DIR}/build.sh --version 1.12.0 --arch cpu
+${DIR}/build.sh --version 1.12.0 --arch gpu
+${DIR}/build.sh --version 1.11 --arch eia
+${DIR}/build.sh --version 1.12 --arch eia
