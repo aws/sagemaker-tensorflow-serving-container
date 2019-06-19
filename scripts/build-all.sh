ff0e499d71b617ee7c5bdca45c6129ca7a7cac8e
icywang86rui <icywang.rui@gmail.com>
2019-06-19 14:01:45 -0700

    feature: add 1.13 EIA containers (#55)

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -14,3 +14,4 @@ ${DIR}/build.sh --version 1.13.0 --arch cpu
 ${DIR}/build.sh --version 1.13.0 --arch gpu
 ${DIR}/build.sh --version 1.11 --arch eia
 ${DIR}/build.sh --version 1.12 --arch eia
+${DIR}/build.sh --version 1.13 --arch eia
