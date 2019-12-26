971c7badf09a10132904e936e80ed22688dd0df8
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2019-12-26 10:57:38 -0800

    Fix for syntax warnings raised by flake8

--- container/sagemaker/deep_learning_container.py
+++ container/sagemaker/deep_learning_container.py
@@ -20,7 +20,7 @@ def _validate_instance_id(instance_id):
     """
     Validate instance ID
     """
-    instance_id_regex = '^(i-\S{17})'
+    instance_id_regex = r'^(i-\S{17})'
     compiled_regex = re.compile(instance_id_regex)
     match = compiled_regex.match(instance_id)
 
@@ -75,7 +75,8 @@ def query_bucket():
     region = _retrieve_instance_region()
 
     if instance_id is not None and region is not None:
-        url = "https://aws-deep-learning-containers-{0}.s3.{0}.amazonaws.com/dlc-containers.txt?x-instance-id={1}".format(region, instance_id)
+        url = ("https://aws-deep-learning-containers-{0}.s3.{0}.amazonaws.com"
+               "/dlc-containers.txt?x-instance-id={1}".format(region, instance_id))
         response = requests_helper(url, timeout=0.2)
 
     logging.debug("Query bucket finished: {}".format(response))
@@ -86,7 +87,7 @@ def query_bucket():
 def requests_helper(url, timeout):
     response = None
     try:
-        response = requests.get(url,timeout=timeout)
+        response = requests.get(url, timeout=timeout)
     except requests.exceptions.RequestException as e:
         logging.error("Request exception: {}".format(e))
 
@@ -97,7 +98,7 @@ def main():
     """
     Invoke bucket query
     """
-    # Logs are not necessary for normal run. Remove this line while debugging.  
+    # Logs are not necessary for normal run. Remove this line while debugging.
     logging.getLogger().disabled = True
 
     logging.basicConfig(level=logging.ERROR)
