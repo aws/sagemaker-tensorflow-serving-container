3732e1907bf828cb72d83861efa72b16d81a3f21
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-01-03 15:20:36 -0800

    Change build spec

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,6 +22,7 @@ docker build \
     --build-arg TFS_SHORT_VERSION=$short_version \
     -f docker/$short_version/Dockerfile.$arch \
     -t $repository:$full_version-$device \
-    -t $repository:$short_version-$device container
+    -t $repository:$short_version-$device \
+    container
 
 remove_ei_executable
