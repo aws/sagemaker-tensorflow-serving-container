3ce9bfdb4bfe6b92f3aec8a55bb54bdf1e5a9847
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2020-04-01 17:54:48 +0100

    replaced deprecated aws ecr get-login by get-login-password

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,11 @@ source scripts/shared.sh
 
 parse_std_args "$@"
 
-$(aws ecr get-login --no-include-email --registry-id $aws_account)
+aws ecr get-login-password --region ${aws_region} \
+    | docker login \
+        --password-stdin \
+        --username AWS \
+        "${aws_account}.dkr.ecr.${aws_region}.amazonaws.com/${repository}"
 docker tag $repository:$full_version-$device $aws_account.dkr.ecr.$aws_region.amazonaws.com/$repository:$full_version-$device
 docker tag $repository:$full_version-$device $aws_account.dkr.ecr.$aws_region.amazonaws.com/$repository:$short_version-$device
 docker push $aws_account.dkr.ecr.$aws_region.amazonaws.com/$repository:$full_version-$device
