d2e11ac1cfcbc088474a04155cf9dd2d7fe941b0
Qian Chen <chqa@amazon.com>
2021-03-19 22:46:47 -0700

    test10

--- docker/build_artifacts/sagemaker/tensorflow-serving.js
+++ docker/build_artifacts/sagemaker/tensorflow-serving.js
@@ -77,8 +77,11 @@ function tfs_json_request(r, json) {
             body = body.replace("\\'instances\\'", "'instances'")
         }
         
-        var content_types = accept == "" ? "" : accept.trim().replace(" ", "").split(",")
-        if (content_types.includes('application/jsonlines') || content_types.includes('application/jsons' == accept)) {
+        if (accept != undefined) {
+            var content_types = accept.trim().replace(" ", "").split(",")
+        }
+        // var content_types = accept == "" ? "" : accept.trim().replace(" ", "").split(",")
+        if (content_types.includes('application/jsonlines') || content_types.includes('application/jsons')) {
             body = body.replace(/\n/g, '')
             r.headersOut['Content-Type'] = content_types[0]
         }
